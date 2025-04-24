444d4736be7c53503323b73d54b5397062438025
RuivoEsquisito <enricobellato00@gmail.com>
2025-04-24 11:30:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ botoes.forEach(botao => {
     botao.classList.toggle("ativo");
   });
 });
+
+/*document.querySelectorAll('.btn-indicador').forEach(botao => {
+  botao.addEventListener('click', () => {
+    fetch('https://seuservidor.com/api/lampada', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ acender: true }) // ou { status: "on" }
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('Lâmpada acionada:', data);
+      botao.classList.toggle('ativo'); // animação visual no botão
+    })
+    .catch(err => {
+      console.error('Erro ao acionar lâmpada:', err);
+    });
+  });
+});
+*/
