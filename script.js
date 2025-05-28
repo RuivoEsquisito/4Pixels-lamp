01411c15161ac2d0d90c266643e0f80df12ac540
RuivoEsquisito <enricobellato00@gmail.com>
2025-05-28 11:36:06 -0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -70,3 +70,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const btnAbrir = document.getElementById('btnAbrirAvaliacao');
+const formContainer = document.getElementById('formAvaliacao');
+
+btnAbrir.addEventListener('click', () => {
+  if (formContainer.style.display === 'none' || formContainer.style.display === '') {
+    formContainer.style.display = 'block';
+    btnAbrir.textContent = 'Fechar Avaliação';
+  } else {
+    formContainer.style.display = 'none';
+    btnAbrir.textContent = 'Deixar Avaliação';
+  }
+});
+
